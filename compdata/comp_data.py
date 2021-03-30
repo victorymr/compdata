98490cfa04df13040da1458424a8167115e72726
victorymr <38334523+victorymr@users.noreply.github.com>
2021-03-29 22:41:31 -0400

    Update comp_data.py

--- compdata/comp_data.py
+++ compdata/comp_data.py
@@ -395,5 +395,6 @@ class Industry:
         #pdb.set_trace()
         table_df = get_table(url,index='industryname')
         #table_df = table_df.set_index('industryname',inplace=True)
+        pdb.set_trace()
         cash = table_df.loc[self.industry]
         return cash
